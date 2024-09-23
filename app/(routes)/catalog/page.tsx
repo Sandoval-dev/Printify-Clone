034db713125ccd67be392535eab852a077e2c8ec
Osman Erdem Kilic <osman_erdem1200@hotmail.com>
2024-09-23 13:56:50 +0300

    React Dropzone

--- app/(routes)/catalog/page.tsx
+++ app/(routes)/catalog/page.tsx
@@ -1,24 +1,108 @@
 'use client'
-import { UploadButton } from '@/lib/uploadthing';
-import React from 'react'
+import { Progress } from '@/components/ui/progress';
+import { useToast } from '@/hooks/use-toast';
+import { UploadButton, useUploadThing } from '@/lib/uploadthing';
+import { cn } from '@/lib/utils';
+import { ImageIcon } from '@radix-ui/react-icons';
+import { Loader2, MousePointerSquareDashed } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import React, { useState, useTransition } from 'react'
+import Dropzone, { useDropzone, FileRejection } from 'react-dropzone'
+
+
 
 const CatalogPage = () => {
+
+  const { toast } = useToast()
+  const [isDragOver, setIsDragOver] = useState<boolean>(false)
+  const [uploadProgress, setUploadProgress] = useState<number>(0)
+
+  const router = useRouter()
+  const [isPending, startTransition] = useTransition()
+
+  const { startUpload, isUploading } = useUploadThing('imageUploader', {
+    onClientUploadComplete: ([data]) => {
+      const configId = data.serverData.configId
+      startTransition(() => {
+        router.push(`/catalog/${configId}`)
+      })
+    },
+    onUploadProgress(p) {
+      setUploadProgress(p)
+    },
+  })
+
+  const onDropAccepted=(acceptedFiles:File[])=>{
+    startUpload(acceptedFiles, {configId:undefined})
+      setIsDragOver(false)
+  }
+
+  const onDropRejected=(rejectedFiles:FileRejection[]) => {
+    const [file]=rejectedFiles
+    setIsDragOver(false)
+
+    toast({
+      title: `${file.file.type} type is not supported`,
+      description: 'Please choose a file with a supported format (png, jpeg, jpg)',
+      variant:'destructive'
+    })
+  }
+
+
   return (
-    <div>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <UploadButton
-          endpoint="imageUploader"
-          onClientUploadComplete={(res) => {
-            // Do something with the response
-            console.log("Files: ", res);
-            alert("Upload Completed");
-          }}
-          onUploadError={(error: Error) => {
-            // Do something with the error.
-            alert(`ERROR! ${error.message}`);
-          }}
-        />
-      </main>
+    <div className='container mx-auto'>
+      <div className={cn('relative h-[500px] flex-1 my-16 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center', {
+        'ring-blue-900/25 bg-blue-900/10': isDragOver
+      })}>
+        <div className='relative flex flex-1 flex-col items-center justify-center w-full'>
+          <Dropzone onDropAccepted={onDropAccepted} onDropRejected={onDropRejected} accept={{
+            'image/png':['.png'],
+            'image/jpeg': ['.jpeg'],
+            'image/jpg': ['.jpg']
+          }} onDragLeave={()=>setIsDragOver(false)} onDragEnter={()=> setIsDragOver(true)}>
+            {({ getRootProps, getInputProps }) => (
+              <div {...getRootProps()} className='relative flex flex-1 flex-col items-center justify-center'>
+                <input {...getInputProps()} />
+                {
+                  isDragOver ? (
+                    <MousePointerSquareDashed className='h-6 w-6 text-zinc-500 mb-2' />
+                  ) : isUploading || isPending ? (
+                    <Loader2 className='animate-spin h-6 w-6 text-zinc-500 mb-2' />
+                  ) : (
+                    <ImageIcon className='h-6 w-6 text-zinc-500 mb-2' />
+                  )
+                }
+                <div className='flex flex-col justify-center mb-2 text-sm text-zinc-700'>
+                  {
+                    isUploading ? (
+                      <div className='flex flex-col items-center'>
+                        <p>Uploading</p>
+                        <Progress value={uploadProgress} className='mt-4 w-44 h-2 bg-gray-400' />
+                      </div>) : isPending ? (
+                        <div className='flex flex-col items-center'>
+                          <p>Redirecting please wait</p>
+                        </div>
+                      ) : isDragOver ? (
+                        <div className='flex flex-col items-center'>
+                          <p>Drop File</p>
+                        </div>
+                      ) : (
+                      <div>
+                          Click to upload or drag and drop
+                      </div>)
+                  }
+                  {
+                    isUploading ? null :(
+                      <p className='text-center'>PNG JPG JPEG</p>
+                    )
+                  }
+                </div>
+              </div>
+            )}
+          </Dropzone>
+        </div>
+      </div>
+
     </div>
 
   )
--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,33 +1,53 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
+import {z} from 'zod'
+import sharp from 'sharp'
+import { prismadb } from "@/lib/prismadb";
 
 const f = createUploadthing();
 
 const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
 
-// FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
+
   imageUploader: f({ image: { maxFileSize: "4MB" } })
-    // Set permissions and file types for this FileRoute
-    .middleware(async ({ req }) => {
-      // This code runs on your server before upload
-      const user = await auth(req);
+  .input(z.object({configId:z.string().optional()}))
+    .middleware(async ({ input }) => {
 
-      // If you throw, the user will not be able to upload
-      if (!user) throw new UploadThingError("Unauthorized");
-
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: user.id };
+      return {input}
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
-      console.log("Upload complete for userId:", metadata.userId);
+       
+        const {configId} = metadata.input
+        const res = await fetch(file.url)
+        const buffer=await res.arrayBuffer()
 
-      console.log("file url", file.url);
+        const imgMetadata= await sharp(buffer).metadata()
+        const {height,width}=imgMetadata
 
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-      return { uploadedBy: metadata.userId };
+        if (!configId) {
+            const configration=await prismadb.configuration.create({
+                data:{
+                    imageUrl:file.url,
+                    height:height || 500,
+                    width:width || 500,
+                }
+            })
+
+            return {configId:configration.id}
+        }else{
+
+            const updateConfigration=await prismadb.configuration.update({
+               where:{
+                id:configId,
+               },
+               data:{
+                croppedImageUrl:file.url
+               }
+            })
+
+            return {configId:updateConfigration.id}
+        }
     }),
 } satisfies FileRouter;
 
