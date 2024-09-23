074c213cdb3613873a3a06ccd216e2b88b7964bc
Osman Erdem Kilic <osman_erdem1200@hotmail.com>
2024-09-23 12:55:35 +0300

    Import Uploadthing

--- app/(routes)/catalog/page.tsx
+++ app/(routes)/catalog/page.tsx
@@ -1,8 +1,26 @@
+'use client'
+import { UploadButton } from '@/lib/uploadthing';
 import React from 'react'
 
 const CatalogPage = () => {
   return (
-    <div>CatalogPage</div>
+    <div>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <UploadButton
+          endpoint="imageUploader"
+          onClientUploadComplete={(res) => {
+            // Do something with the response
+            console.log("Files: ", res);
+            alert("Upload Completed");
+          }}
+          onUploadError={(error: Error) => {
+            // Do something with the error.
+            alert(`ERROR! ${error.message}`);
+          }}
+        />
+      </main>
+    </div>
+
   )
 }
 
